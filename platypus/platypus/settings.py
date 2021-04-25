4af63bc1f7d35115bc127e531593eecd6a66b7ac
Aiden Kwak <duckracoon@gist.ac.kr>
2021-04-26 03:27:44 +0900

    SECRET_KEY 보안문제 commit

--- platypus/platypus/settings.py
+++ platypus/platypus/settings.py
@@ -11,16 +11,26 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ, os
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# reading .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jzp(jsxrnii-%1m!xgpxk3hq%kv%&1b@(x%gcicif%=9)v29zp'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
